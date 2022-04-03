30587b9cc3d4d7ac882cb07e8be5e6736c12bc4d
TakumaNakao <takuma120710@gmail.com>
2022-04-04 01:49:09 +0900

    no message

--- Test/test.cs
+++
@@ -1,9 +0,0 @@
-[TestClass]
-    public class UnitTestTempDataFunction
-    {
-        [TestMethod]
-        public void CanGetTempData()
-        {
-            Assert.AreEqual(1, 1);
-        } 
-    }
