bce484340fdb1c835dc94111be2d1bbb0e4d165e
TakumaNakao <takuma120710@gmail.com>
2022-04-04 00:31:09 +0900

    testファイルを追加

---
+++ Test/test.cs
@@ -0,0 +1,9 @@
+[TestClass]
+    public class UnitTestTempDataFunction : FunctionTestHelper.FunctionTest
+    {
+        [TestMethod]
+        public void CanGetTempData()
+        {
+            Assert.AreEqual(1, 1);
+        } 
+      }
